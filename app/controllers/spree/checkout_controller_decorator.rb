201a8b10e2a28677d50a32f14119013e16031777
Pavel Brylov <pavel@brilow.info>
2012-06-26 01:38:22 +0300

    Rails.application.config instead of Rails.config in rails 3

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -12,13 +12,13 @@ module Spree
 
       if payment_method.kind_of? Spree::Gateway::Seb
         if !request.ssl? && Rails.env =~ /production/
-          redirect_to gateway_seb_path(:order_id => @order.number, :protocol => "https", :host => Rails.configuration.main_domain)
+          redirect_to gateway_seb_path(:order_id => @order.number, :protocol => "https", :host => Rails.application.config.main_domain)
         else
           redirect_to gateway_seb_path(:order_id => @order.number)
         end
       elsif payment_method.kind_of? Spree::Gateway::Swedbank
         if !request.ssl? && Rails.env =~ /production/
-          redirect_to gateway_swedbank_path(:order_id => @order.number, :protocol => "https", :host => Rails.configuration.main_domain)
+          redirect_to gateway_swedbank_path(:order_id => @order.number, :protocol => "https", :host => Rails.application.config.main_domain)
         else
           redirect_to gateway_swedbank_path(:order_id => @order.number)
         end
